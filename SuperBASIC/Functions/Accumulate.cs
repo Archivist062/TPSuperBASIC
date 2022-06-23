2584ace53a24c348cd97f5bd05297801268ca826
Ludovic Lagouardette <ludovic@nekoit.xyz>
2022-06-23 14:27:52 +0200

    Accumulate fonctionne

---
+++ SuperBASIC/Functions/Accumulate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    public class Accumulate : IFunction
+    {
+        static public float Function(short indexStart, short indexEnd)
+        {
+            return Memory.ValuesBetween(indexStart, indexEnd).Sum();
+        }
+
+        float IFunction.Apply(List<BasicNumber> arguments)
+        {
+            // Verifier le nombre d'arguments
+            if (arguments.Count != 2) return float.NaN ;
+            var toFloat = arguments.Select(x => x.GetValue());
+
+            // Verifier que les arguments sont compatibles
+            if (toFloat.All(x => x == Math.Floor(x))) {
+                if (toFloat.All(x => x <= Int16.MaxValue))
+                {
+                    var toInt = toFloat.Select(x => (short)x);
+
+                    return Function(toInt.First(), toInt.Last());
+                }
+            }
+            return float.NaN;
+        }
+    }
+}
---
+++ TestsSuperBASIC/TestAccumulate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperBASIC;
+using SuperBASIC.Functions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsSuperBASIC
+{
+    [TestClass]
+    public class TestAccumulate
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+			Library lib = new Library();
+            var output = new Mock.MockPrint();
+            lib.AddFunction(output, 1, "PRINT");
+
+            lib.AddFunction(new MemoryStore(), 2, "MEMSTORE");
+            lib.AddFunction(new Accumulate(), 2, "ACCUMULATE");
+
+            Runtime r = new Runtime(lib);
+			r.OpenFile(Directory.GetCurrentDirectory() + "\\CasDeTest\\TestAccumulate-MyTestMethod.basic");
+			r.Run();
+			
+			Assert.AreEqual(1f+2f+3f+4f, output.output[0]);
+		}
+    }
+}
